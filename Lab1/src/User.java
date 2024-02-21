17ed207fcd244f437b24ce79c013daed869180a8
markiianhyz1 <127431888+markiianhyz1@users.noreply.github.com>
2024-02-21 16:22:00 +0200

    Add files via upload

---
+++ Lab1/src/User.java
@@ -0,0 +1,18 @@
+
+public class User {
+    int id;
+    int age;
+    String name;
+    String surname;
+    double weight;
+    double height;
+
+    public User(int id, int age, String name, String surname, double weight, double height) {
+        this.id = id;
+        this.age = age;
+        this.name = name;
+        this.surname = surname;
+        this.weight = weight;
+        this.height = height;
+    }
+}
